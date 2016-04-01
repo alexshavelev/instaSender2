a269b01b75c9624d8ad8e467e08687b33f83b757
ashavelev <1992Samsung>
2016-04-01 16:52:22 +0200

    add telegram sender

--- src/main/java/com/shavelev/alexander/workers/MainWorker.java
+++ src/main/java/com/shavelev/alexander/workers/MainWorker.java
@@ -6,20 +6,26 @@ package com.shavelev.alexander.workers;
 
 import com.shavelev.alexander.workers.UpdateWorker;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 public class MainWorker {
     private static final Logger LOG = Logger.getLogger(String.valueOf(UpdateWorker.class));
+    private static boolean isWork = true;
     private UpdateWorker updateWorker;
+    private int hours = new Date().getHours();
 
     public MainWorker() {
         this.updateWorker = new UpdateWorker();
     }
 
     public void startWorker() throws InterruptedException {
-        while (true){
+        while (isWork){
             try {
                 this.updateWorker.update();
+                if (Math.abs((new Date().getHours() - hours)) > 3) {
+                    this.updateWorker.cleanState();
+                }
             } catch (Exception e) {
                 LOG.info("We missed 1 update");
             }
@@ -27,4 +33,6 @@ public class MainWorker {
             Thread.sleep(300000);
         }
     }
+
+
 }
--- src/main/java/com/shavelev/alexander/workers/UpdateWorker.java
+++ src/main/java/com/shavelev/alexander/workers/UpdateWorker.java
@@ -1,11 +1,23 @@
 package com.shavelev.alexander.workers;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramBotAdapter;
+import com.pengrad.telegrambot.model.ReplyKeyboardMarkup;
+import com.pengrad.telegrambot.*;
 import com.shavelev.alexander.helpers.AuthMsg;
 //import com.google.gson.*;
 import net.sf.json.*;
+import org.telegram.telegrambots.TelegramBotsApi;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+
+
+
+
 
 
 /**
@@ -16,24 +28,50 @@ public class UpdateWorker {
     private final String MAIN_URL = "https://api.instagram.com/";
     private final String SELF_FEED = "v1/users/self/feed";
     private final String ACCESS_TOKEN = "30782247.1fb234f.85987a7aa25842a69bb4165a38e4f2ac";
+    private final String TELEGRAM_TOKEN = "99434746:AAETpph94f6EIIBY-reY6CNoJp50nw-fdoQ";
+    private final int alexShavelev = 64605342;
+    private final int bastards = -133878532;
     private String httpResult;
+    private ArrayList<String> linksList;
+    private TelegramBot bot = TelegramBotAdapter.build(TELEGRAM_TOKEN);
+
+
+    public UpdateWorker() {
+        this.linksList = new ArrayList<>();
+    }
+
+    public void cleanState() {
+        this.linksList.clear();
+    }
 
     public void update() throws Exception {
         String url = MAIN_URL  + SELF_FEED + "?access_token=" + ACCESS_TOKEN;
         System.out.println("url=" + url);
         httpResult = HttpUrlConnection.sendGet(url);
-        System.out.println("res=" + httpResult);
-//        UpdateWorker x = new Gson().fromJson(httpResult, UpdateWorker.class);
-//        System.out.println("x=" + x);
 
         JSONObject obj = parseJson(httpResult);
         JSONArray data = obj.getJSONArray("data");
 
-//        Map<String, String> result = new HashMap<String, String>();
+
         for (int i = 0; i < data.size(); i++) {
             JSONObject op = data.getJSONObject(i);
             String link = op.getString("link");
-            System.out.println("link="+link);
+
+            if (!linksList.contains(link)) {
+                System.out.println("link="+link);
+
+                bot.sendMessage(
+                                alexShavelev,
+                                link,
+                                false,
+                                1,
+                        new ReplyKeyboardMarkup(new String[]{"ok", "cancel"}));
+                bot.sendMessage(bastards, link, false, 1, new ReplyKeyboardMarkup(new String[]{"ok", "cancel"}));
+                // send to telegram
+                linksList.add(link);
+            } else {
+                System.out.println("skip");
+            }
         }
     }
 
